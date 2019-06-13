e3461f993db583340e370bfa2c5bc0f99a0bcd80
Kyle Allan <kyle@stitchdata.com>
2019-06-13 13:16:37 +0000

    license; copyright; bump to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-liveperson',
-      version='0.0.1',
+      version='0.0.2',
       description='Singer.io tap for extracting data from the LivePerson API',
       author='Fishtown Analytics',
       url='http://fishtownanalytics.com',
