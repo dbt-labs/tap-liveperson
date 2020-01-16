3281121cd5e57ff60790453fc401fc0bd898e1a6
Jacob Baca <52418765+jacobrobertbaca@users.noreply.github.com>
2020-01-16 16:39:01 -0500

    bumped version to 0.0.7 (#7)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-liveperson',
-      version='0.0.6',
+      version='0.0.7',
       description='Singer.io tap for extracting data from the LivePerson API',
       author='Fishtown Analytics',
       url='http://fishtownanalytics.com',
