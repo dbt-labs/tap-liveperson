0cdcbce48ef14eaf6358e6981631ab80a93724cd
Tomek Zbrozek <tomasz.zbrozek@gmail.com>
2020-01-16 22:35:37 +0100

    Coerce timestamp-id to string (#6)
    * Coerce timestamp-id to string
    
    * Fix coercion to string

--- tap_liveperson/streams/agent_state_distribution.py
+++ tap_liveperson/streams/agent_state_distribution.py
@@ -28,7 +28,7 @@ class AgentStateDistribution(RealtimeStream):
         ]
 
         for record in transformed:
-            record['id'] = self.get_pk_value(record)
+            record['id'] = str(self.get_pk_value(record))
 
         return transformed
 
