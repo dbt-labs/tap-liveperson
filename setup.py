82a14f92d0027a02bf62cfa62fb0a0fdb31a9526
Nick McCoy <33731945+nick-mccoy@users.noreply.github.com>
2020-01-31 13:38:49 -0500

    bump version to 1.0.0 and add circle file (#8)
    * add circle file
    
    * bump version to 1.0.0
    
    * add json validator

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-liveperson',
-      version='0.0.7',
+      version='1.0.0',
       description='Singer.io tap for extracting data from the LivePerson API',
       author='Fishtown Analytics',
       url='http://fishtownanalytics.com',
